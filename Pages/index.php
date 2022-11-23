887fe1e9a03f380eef08c9e1152bbcdd83eb3876
Seyon Suriyakumaran <83668877+SeyonSuriya@users.noreply.github.com>
2022-11-23 15:25:12 +0530

    Update index.php

--- Pages/index.php
+++ Pages/index.php
@@ -1,1 +1,29 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" type="text/css" href="style.php">
+    <title>Home Page</title>
+</head>
+<body>
+    <div class="top">
+        <h1>My Friend System</h1>
+    </div>
+    <div class = "box">
+            <h2>Practical Exam :: Home Page</h2>
+            <p>Name: Seyon Suriyakumaran &emsp;&emsp; Student ID: SE/2019/001</p>
+            <p>Email: suriyaseyon6@gmail.com</p>
+            <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source.</p>
+            <p>I have not worked collaboratively nor have I copied from any other student's work.</p>
+            <button onclick="document.location='signup.php'">Register</button>
+            <button onclick="document.location='login.php'">Login</button>
+            
+    </div>
+    <br>
+    <div class="top">
+        <h3>copyright &#xa9; setu.kln.ac.lk</h3>
+    </div>
+</body>
+</html>
